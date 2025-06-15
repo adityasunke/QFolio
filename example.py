cccbefc316786c9f3bf7c91abdf13d9c8840d32d
adityasunke2004 <adityasunke2004@vt.edu>
2025-06-15 16:37:31 -0400

    Installed Packages and Ran an example
    Installed all necessary packages and ran the example from the Qiskit Portfolio Optimization page

---
+++ example.py
@@ -0,0 +1,62 @@
+from qiskit.circuit.library import TwoLocal
+from qiskit.result import QuasiDistribution
+from qiskit_aer.primitives import Sampler
+from qiskit_algorithms import NumPyMinimumEigensolver, QAOA, SamplingVQE
+from qiskit.primitives import BaseSamplerV1
+from qiskit_algorithms.optimizers import COBYLA
+from qiskit_finance.applications.optimization import PortfolioOptimization
+from qiskit_finance.data_providers import RandomDataProvider
+from qiskit_optimization.algorithms import MinimumEigenOptimizer
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+
+# set number of assets (= number of qubits)
+num_assets = 4
+seed = 123
+
+# Generate expected return and covariance matrix from (random) time-series
+stocks = [("TICKER%s" % i) for i in range(num_assets)]
+data = RandomDataProvider(
+    tickers=stocks,
+    start=datetime.datetime(2016, 1, 1),
+    end=datetime.datetime(2016, 1, 30),
+    seed=seed,
+)
+data.run()
+mu = data.get_period_return_mean_vector()
+sigma = data.get_period_return_covariance_matrix()
+
+plt.imshow(sigma, interpolation="nearest")
+plt.show()
+
+q = 0.5  # set risk factor
+budget = num_assets // 2  # set budget
+penalty = num_assets  # set parameter to scale the budget penalty term
+
+portfolio = PortfolioOptimization(
+    expected_returns=mu, covariances=sigma, risk_factor=q, budget=budget
+)
+qp = portfolio.to_quadratic_program()
+print(qp)
+
+def print_result(result):
+    selection = result.x
+    value = result.fval
+    print("Optimal: selection {}, value {:.4f}".format(selection, value))
+
+    eigenstate = result.min_eigen_solver_result.eigenstate
+    probabilities = (
+        eigenstate.binary_probabilities()
+        if isinstance(eigenstate, QuasiDistribution)
+        else {k: np.abs(v) ** 2 for k, v in eigenstate.to_dict().items()}
+    )
+    print("\n----------------- Full result ---------------------")
+    print("selection\tvalue\t\tprobability")
+    print("---------------------------------------------------")
+    probabilities = sorted(probabilities.items(), key=lambda x: x[1], reverse=True)
+
+    for k, v in probabilities:
+        x = np.array([int(i) for i in list(reversed(k))])
+        value = portfolio.to_quadratic_program().objective.evaluate(x)
+        print("%10s\t%.4f\t\t%.4f" % (x, value, v))
